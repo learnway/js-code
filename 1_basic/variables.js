65eaf8782587da228488100b73e8e684a17842e2
learnway <44905838+learnway@users.noreply.github.com>
2024-10-09 10:39:42 +0000

    variables

---
+++ 1_basic/variables.js
@@ -0,0 +1,12 @@
+const productId = 3333;
+let productEmail = "hello@gmail.com";
+var productName = "NewAI";
+productDetails = "OK";  // wrong way to varriable declare
+
+// productId = 22;          // never change
+productEmail = "new@gmail.com" // change
+productName = "aone"
+productDetails = "Lol"
+// console.log(productEmail);
+
+console.table([productId, productEmail, productName, productDetails]);
