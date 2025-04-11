552042fb698fdc6e10a06da7b39a97a7d36bf035
learnway <44905838+learnway@users.noreply.github.com>
2025-04-11 11:25:34 +0000

    if statement

--- 1_basic/13_if_else_statement.js
+++ 1_basic/13_if_else_statement.js
@@ -2,3 +2,10 @@ x = 10; y = 20;
 if (x > y) {
     console.log("x is grater than y");
 }
+else if (x < y) {
+    console.log("x is less than y");
+}
+else {
+    console.log("x is equal to y");
+}
+// Output: x is less than y
